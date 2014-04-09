3dab51fbaea42c4aa8fc2e054cd6dd417e6c8720
Gabor de Mooij <gabordemooij@gmail.com>
2014-04-09 20:22:50 +0200

    Updated version string.

--- StampTE.php
+++ StampTE.php
@@ -16,7 +16,7 @@
  *  Stamp t.e.
  *  The Beautiful Template Engine
  *  @author Gabor de Mooij
- *  @version 2.1.5
+ *  @version 2.2
  *  @copyright 2013
  *  @license New BSD License
  *  ---------------------------------------------------------------------------
